5b694c9346b2534a2845953627cf09d3bdf7f6be
Beda Kosata <beda@zirael.org>
2005-07-21 08:40:35 +0000

    small fixes
    git-archimport-id: beda@zirael.org--2004/bkchem-plugins--mainline--0.1.0--patch-7

--- angle_between_bonds.py
+++ angle_between_bonds.py
@@ -1,15 +1,16 @@
 
 import geometry
 import math
+from singleton_store import Store
 
 bs = [b for b in App.paper.selected if b.object_type == "bond"]
 if not len( bs) == 2:
-  App.log( "You have to have 2 bonds selected", message_type="hint")
+  Store.log( "You have to have 2 bonds selected", message_type="hint")
 else:
   angs = [geometry.clockwise_angle_from_east( b.atom1.x-b.atom2.x, b.atom1.y-b.atom2.y) for b in bs]
   a1, a2 = angs
   ret = a1 > a2 and (a1-a2) or (a2-a1)
-  App.log( str( 180*ret/math.pi))
+  Store.log( str( 180*ret/math.pi))
 
 
 
